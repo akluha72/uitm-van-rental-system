7e21cdee8014f3744a705fbdda842a7d3e3b92c6
lutfii <lutfiakmal72@myra.com.my>
2024-12-31 00:48:03 +0800

    landing page wip

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('landing');
+})->name('landing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
